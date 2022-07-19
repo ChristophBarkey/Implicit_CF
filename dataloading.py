cf75583055c1b4c64e55770d0a9e724e91146236
ChristophBarkey <c_barkey@web.com>
2022-07-19 23:50:49 +0200

    updated import_agco function

--- dataloading.py
+++ dataloading.py
@@ -10,33 +10,40 @@ class DataLoader:
         pass
 
     # integrated func to load co(d) data and return csr user_item
-    def import_agco(self, file, clip) :
+    def import_agco(self, file, return_type) :
         #import pandas as pd
-        locations = pd.read_csv('locations_agco_new.csv', sep='|', low_memory=False)
+        agco_loc = pd.read_csv('loc_agco_new.csv', sep = '|', low_memory=False)
         if file == 'CO':
-            co = pd.read_csv('co_agco_new.csv', sep = '|', low_memory=False)
-            cd = pd.read_csv('cd_agco_new.csv', sep = '|', low_memory=False)
-            cod = pd.merge(co, cd, on='co_id', how='inner')
-            cod_loc = pd.merge(cod, locations, left_on='supply_location_id', right_on='location_id', how='left')
-            dealer_items = cod_loc[['group1', 'item_id', 'requested_quantity']]
+            agco_cod = pd.read_csv('cod_agco_new.csv', sep = '|', low_memory=False)
+            agco_cod_loc = pd.merge(agco_cod, agco_loc, left_on='supply_location_id', right_on='location_id', how='inner')
+            user_item_cod_loc = agco_cod_loc[['user', 'item_id', 'requested_quantity']].groupby(by=['user', 'item_id']).sum().reset_index()
+            user_item_cod_loc = user_item_cod_loc[user_item_cod_loc.requested_quantity >= 1]
+            purchases_clipped_co = np.percentile(user_item_cod_loc.requested_quantity, 99)
+            user_item_cod_loc['purchases'] = np.clip(user_item_cod_loc.requested_quantity, a_min=1, a_max=purchases_clipped_co)
+            user_item_co = user_item_cod_loc[['user', 'item_id', 'purchases']]
+            user_item_co.columns = ['user', 'item', 'purchases']
+            df = user_item_co
         
         if file == 'PO':
-            po = pd.read_csv('po_agco_new.csv', sep = '|', low_memory=False)
-            ppd = pd.read_csv('pd_agco_new.csv', sep = '|', low_memory=False)
-            pod = pd.merge(po, ppd, on='po_id', how='inner')
-            pod_loc = pd.merge(pod, locations, left_on='receive_location_id', right_on='location_id', how='left')
-            dealer_items = pod_loc[['group1', 'item_id', 'requested_quantity']]
+            agco_pod = pd.read_csv('pod_agco_new.csv', sep = '|', low_memory=False)
+            agco_pod_loc = pd.merge(agco_pod, agco_loc, left_on='receive_location_id', right_on='location_id', how='inner')
+            user_item_pod_loc = agco_pod_loc[['user', 'item_id', 'requested_quantity']].groupby(by=['user', 'item_id']).sum().reset_index()
+            user_item_pod_loc = user_item_pod_loc[user_item_pod_loc.requested_quantity >= 1]
+            purchases_clipped_po = np.percentile(user_item_pod_loc.requested_quantity, 99)
+            user_item_pod_loc['purchases'] = np.clip(user_item_cod_loc.requested_quantity, a_min=1, a_max=purchases_clipped_po) 
+            user_item_po = user_item_pod_loc[['user', 'item_id', 'purchases']]
+            user_item_po.columns = ['user', 'item', 'purchases']
+            df = user_item_po
         
-        user_item = dealer_items.groupby(by=['group1', 'item_id']).sum().reset_index()
-        user_item.columns = ['user', 'item', 'purchases']
-        user_item = user_item.loc[user_item.purchases > 0]
-        if clip < 100 :
-            user_item['purchases'] = np.clip(user_item.purchases, a_min=1, a_max=np.percentile(user_item.purchases, clip))
-        user_item['user'] = pd.Categorical(user_item.user).codes
-        user_item['item'] = pd.Categorical(user_item.item).codes
-        user_item_coo = coo_matrix((user_item.purchases, (user_item.user, user_item.item)))
-        user_item_csr = user_item_coo.tocsr()
-        return user_item_csr
+        if return_type == 'df':
+            return df
+        
+        if return_type == 'csr':
+            df['user'] = pd.Categorical(df.user).codes
+            df['item'] = pd.Categorical(df.item).codes
+            user_item_coo = coo_matrix((df.purchases, (df.user, df.item)))
+            user_item_csr = user_item_coo.tocsr()
+            return user_item_csr
 
     # func to get the number of items each user has interacted with
     def items_per_user(self, csr):
