44205f4e7f0115b5922971e8ed7c5cc21a73597a
ChristophBarkey <c_barkey@web.com>
2022-08-11 11:10:54 +0200

    moved show loss to fit

--- eALS_adaptor.py
+++ eALS_adaptor.py
@@ -5,20 +5,18 @@ import multiprocessing
 from implicit.cpu.matrix_factorization_base import MatrixFactorizationBase
 
 class eALSAdaptor(MatrixFactorizationBase):
-    def __init__(self, num_threads=0, show_loss=False, *args, **kwargs):
+    def __init__(self, num_threads=0, *args, **kwargs):
         super(eALSAdaptor, self).__init__()
 
         # create a LightFM model using the supplied parameters
         from eals import ElementwiseAlternatingLeastSquares
         self.model = ElementwiseAlternatingLeastSquares(*args, **kwargs)
 
-        self.show_loss = show_loss
         self.num_threads = num_threads or multiprocessing.cpu_count()
 
-    def fit(self, user_item):
+    def fit(self, user_item, show_loss=False):
         # fit the wrapped model
-        self.model.fit(user_item, 
-                        show_loss=self.show_loss)
+        self.model.fit(user_item, show_loss=show_loss)
    
         # convert model attributes back to this class, so that
         # the recommend/similar_items etc calls on the base class will work
