50d9c78ecda0c3b2c28a50658c763c1bb3a560ec
ChristophBarkey <c_barkey@web.com>
2022-08-20 17:52:24 +0200

    updated class

--- datapreparation_py.py
+++ datapreparation_py.py
@@ -1,6 +1,7 @@
 # %%
 import pandas as pd
 from lightfm.data import Dataset
+import scipy.sparse as sparse
 
 class DataPreparation:
 
@@ -14,7 +15,30 @@ class DataPreparation:
         """
         self.user_item = user_item
 
-    def _get_user_features(self):
+
+
+
+
+    def get_input_data(self, features):
+        """" Function to get the input data for a LightFM model mmm
+
+        Returns
+        -------
+        (interactions, weights, user_features) : tuple(coo_matrix, coo_matrix, csr_matrix)
+            Data prepared for input for LightFM model
+        """
+
+        dataset_t = Dataset()
+        dataset_t.fit(self.user_item.user.unique(), self.user_item.item.unique())
+
+        interactions_matrix, weights_matrix = dataset_t.build_interactions([tuple(i) for i in self.user_item.values])
+        
+        user_features_sp = self._get_user_features(features)
+
+        return (interactions_matrix, weights_matrix, user_features_sp)
+
+
+    def _get_user_features(self, features):
         """" Helper function to get the user_feature values and the user_feature list
 
         Returns
@@ -26,39 +50,53 @@ class DataPreparation:
         user_features_terex = pd.read_csv('user_features_terex_new.csv', sep='|')
         available_dealers = self.user_item[['user']].drop_duplicates()
         user_features_terex = pd.merge(available_dealers, user_features_terex, left_on='user', right_on='dealer', how='left')
-        uf_t = user_features_terex[['dealer', 'currency', 'brand']].drop_duplicates(['dealer']).dropna().set_index('dealer')
         
-        # list of all distinct feature values/categories over all features
-        # input for data.fit() method
-        user_features = []
-        for c in uf_t.columns:
-            col = uf_t[str(c)]
-            for v in col.unique():
-                user_features.append(v)
+        user_features = self._aggregate_features(user_features_terex, features)
 
-        # list of users and tuples of respective feature values
-        # input for data.build_user_features() method
-        feature_list = []
-        for i in uf_t.index:
-            feature_list.append(list(uf_t.loc[str(i), :]))
-        user_tuple = list(zip(uf_t.index, feature_list))
-        return (user_features, user_tuple)
+        return user_features
 
-    def get_input_data(self):
-        """" Function to get the input data for a LightFM model
+    def _aggregate_features(self, user_features_file, features):   
+        for feature in features:
+            uf_temp = user_features_file[['dealer', feature]].drop_duplicates(['dealer', feature]).dropna().set_index('dealer')
+            
+            user_features = []
+            for c in uf_temp.columns:
+                col = uf_temp[str(c)]
+                for v in col.unique():
+                    user_features.append(v)
 
-        Returns
-        -------
-        (interactions, weights, user_features) : tuple(coo_matrix, coo_matrix, csr_matrix)
-            Data prepared for input for LightFM model
-        """
-        user_features, user_tuple = self._get_user_features()
-        dataset_t = Dataset()
-        dataset_t.fit(self.user_item.user.unique(), self.user_item.item.unique(), user_features)
+            feature_list = []
+            for i in range(len(uf_temp)):
+                feature_list.append(list(uf_temp.iloc[i, :]))
+            user_tuple = list(zip(uf_temp.index, feature_list))   
 
-        interactions_matrix, weights_matrix = dataset_t.build_interactions([tuple(i) for i in self.user_item.values])
-        user_features_sp = dataset_t.build_user_features(user_tuple, normalize= False)
-        return (interactions_matrix, weights_matrix, user_features_sp)
+            dataset_t = Dataset()
+            dataset_t.fit(self.user_item.user.unique(), self.user_item.item.unique(), user_features)
 
+            user_features_sp = dataset_t.build_user_features(user_tuple, normalize=False)
+
+            user_features_sp_norm = self._normalize(user_features_sp)
+
+            if feature == features[0]:
+                user_features_sp_ret = user_features_sp_norm
+            else:
+                user_features_sp_ret = self._add_csr(user_features_sp_ret, user_features_sp_norm)
+        return user_features_sp_ret
+
+    def _normalize(self, csr):
+        lil = csr.tolil()
+        for i in range(csr.shape[0]):
+            array = csr[i, csr.shape[0]:].toarray()
+            if array.sum() == 0:
+                norm_array = array
+            else:
+                norm_array = array / array.sum()
+            lil[i, csr.shape[0]:] = norm_array
+        return lil.tocsr()
+
+    def _add_csr(self, csr, add):
+        for i in range((add.shape[1]-add.shape[0])):
+            csr = sparse.hstack((csr, add[:,(add.shape[0]+i)].A.T[0][:,None]))
+        return csr
 
 
