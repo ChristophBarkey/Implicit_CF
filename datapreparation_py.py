4d91809c1ec1e8919a9a7c052a883a7d94427055
ChristophBarkey <c_barkey@web.com>
2022-08-20 20:37:23 +0200

    added item feature func

--- datapreparation_py.py
+++ datapreparation_py.py
@@ -19,7 +19,7 @@ class DataPreparation:
 
 
 
-    def get_input_data(self, features):
+    def get_input_data(self, user_features=None, item_features=None):
         """" Function to get the input data for a LightFM model mmm
 
         Returns
@@ -32,10 +32,18 @@ class DataPreparation:
         dataset_t.fit(self.user_item.user.unique(), self.user_item.item.unique())
 
         interactions_matrix, weights_matrix = dataset_t.build_interactions([tuple(i) for i in self.user_item.values])
-        
-        user_features_sp = self._get_user_features(features)
 
-        return (interactions_matrix, weights_matrix, user_features_sp)
+        return_list = [interactions_matrix, weights_matrix]
+        
+        if user_features is not None:
+            user_features_sp = self._get_user_features(user_features)
+            return_list.append(user_features_sp)
+
+        if item_features is not None:
+            item_features_sp = self._get_item_features(item_features)
+            return_list.append(item_features_sp)
+
+        return return_list
 
 
     def _get_user_features(self, features):
@@ -51,37 +59,73 @@ class DataPreparation:
         available_dealers = self.user_item[['user']].drop_duplicates()
         user_features_terex = pd.merge(available_dealers, user_features_terex, left_on='user', right_on='dealer', how='left')
         
-        user_features = self._aggregate_features(user_features_terex, features)
+        user_features_ret = self._aggregate_features(user_features_terex, features)
 
-        return user_features
+        return user_features_ret
 
-    def _aggregate_features(self, user_features_file, features):   
+    def _get_item_features(self, features):
+        """" Helper function to get the user_feature values and the user_feature list
+
+        Returns
+        -------
+        (user_features, user_tuple) : tuple(list, list)
+            lists containing the data accordingly
+        """
+    
+        items_terex = pd.read_csv('items_terex_new.csv', sep='|')
+        available_items = self.user_item[['item']].drop_duplicates()
+        
+        item_features = pd.merge(available_items, items_terex, left_on='item', right_on='item_id', how='left')
+        skus_terex = pd.read_csv('skus_terex_new.csv', sep='|', low_memory=False)
+
+        item_sku_features = pd.merge(item_features, skus_terex, on='item_id', how='left')
+        
+        item_features_ret = self._aggregate_features(item_sku_features, features)
+
+        return item_features_ret
+
+    def _aggregate_features(self, features_file, features, instance):   
         for feature in features:
-            uf_temp = user_features_file[['dealer', feature]].drop_duplicates(['dealer', feature]).dropna().set_index('dealer')
+            if instance == 'user':
+                uif_temp = features_file[['dealer', feature]].drop_duplicates(['dealer', feature]).dropna().set_index('dealer')
+            if instance == 'item':
+                uif_temp = features_file[['item', feature]].drop_duplicates(['item', feature]).dropna().set_index('item')
             
-            user_features = []
-            for c in uf_temp.columns:
-                col = uf_temp[str(c)]
-                for v in col.unique():
-                    user_features.append(v)
+            ui_features = self._gather_features(uif_temp)
 
-            feature_list = []
-            for i in range(len(uf_temp)):
-                feature_list.append(list(uf_temp.iloc[i, :]))
-            user_tuple = list(zip(uf_temp.index, feature_list))   
+            ui_tuple = self._get_feature_tuple(uif_temp)
 
             dataset_t = Dataset()
-            dataset_t.fit(self.user_item.user.unique(), self.user_item.item.unique(), user_features)
+            dataset_t.fit(self.user_item.user.unique(), self.user_item.item.unique(), ui_features)
+            
+            if instance == 'user':
+                ui_features_sp = dataset_t.build_user_features(ui_tuple, normalize=False)
+            if instance == 'item':
+                ui_features_sp = dataset_t.build_item_features(ui_tuple, normalize=False)
 
-            user_features_sp = dataset_t.build_user_features(user_tuple, normalize=False)
 
-            user_features_sp_norm = self._normalize(user_features_sp)
+            ui_features_sp_norm = self._normalize(ui_features_sp)
 
             if feature == features[0]:
-                user_features_sp_ret = user_features_sp_norm
+                ui_features_sp_ret = ui_features_sp_norm
             else:
-                user_features_sp_ret = self._add_csr(user_features_sp_ret, user_features_sp_norm)
-        return user_features_sp_ret
+                ui_features_sp_ret = self._add_csr(ui_features_sp_ret, ui_features_sp_norm)
+        return ui_features_sp_ret
+    
+    def _gather_features(self, feature_file):
+        features = []
+        for c in feature_file.columns:
+            col = feature_file[str(c)]
+            for v in col.unique():
+                features.append(v)
+        return features
+
+    def _get_feature_tuple(self, feature_file):
+        feature_list = []
+        for i in range(len(feature_file)):
+            feature_list.append(list(feature_file.iloc[i, :]))
+        feature_tuple = list(zip(feature_file.index, feature_list))   
+        return feature_tuple
 
     def _normalize(self, csr):
         lil = csr.tolil()
