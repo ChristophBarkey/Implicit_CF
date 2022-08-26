484ae49f2c1749456adce12dc3b83a0825cbb557
ChristophBarkey <c_barkey@web.com>
2022-08-26 16:08:32 +0200

    added methods to support csr splitting

--- datapreparation_py.py
+++ datapreparation_py.py
@@ -1,10 +1,23 @@
 # %%
 import pandas as pd
+import numpy as np
 from lightfm.data import Dataset
 import scipy.sparse as sparse
 
 class DataPreparation:
 
+    #def __init__(self, user_item):
+        """"
+        Parameters
+        ----------
+        user_item : Pandas Dataframe
+            Dataframe with the columns ['user', 'item', 'purchases'] 
+            Containing the user-item interactions
+        """
+    #    self.user_item = user_item
+
+
+    # NEW TEST
     def __init__(self, user_item):
         """"
         Parameters
@@ -14,12 +27,43 @@ class DataPreparation:
             Containing the user-item interactions
         """
         self.user_item = user_item
+        self.user_item_full = self._get_full_user_item()
+        self.users = self.user_item_full[['item', 'item_codes']].drop_duplicates(['item', 'item_codes']).sort_values(['item_codes']).item
+        self.items = self.user_item_full[['user', 'user_codes']].drop_duplicates(['user', 'user_codes']).sort_values(['user_codes']).user
+
+    # NEW TEST
+    def _get_full_user_item(self):
+        user_item_full = self.user_item.copy()
+        user_item_full['user_codes'] = pd.Categorical(user_item_full.user).codes
+        user_item_full['item_codes'] = pd.Categorical(user_item_full.item).codes
+        return uesr_item_full
+
+    # NEW TEST
+    def get_interaction_data(self):
+        df = self.user_item_full.copy()
+        df['ones'] = 1
+        weights_coo = coo_matrix((df.purchases, (df.user_codes, df.item_codes)))
+        interactions_coo = coo_matrix((df.ones, (df.user_codes, df.item_codes)))
+        weights_csr = weights_coo.tocsr()
+        interactions_csr = interactions_coo.tocsr()
+        return (interactions_csr, weights_csr)
+
+    def get_feature_input(self, OEM='TEREX', user_features=None, item_features=None):
+
+        return_list = []
+        
+        if user_features is not None:
+            user_features_sp = self._get_user_features(OEM, user_features)
+            return_list.append(user_features_sp)
+
+        if item_features is not None:
+            item_features_sp = self._get_item_features(OEM, item_features)
+            return_list.append(item_features_sp)
+
+        return return_list
 
 
-
-
-
-    def get_input_data(self, user_features=None, item_features=None):
+    def get_input_data(self, OEM, user_features=None, item_features=None):
         """" Function to get the input data for a LightFM model
         Parameters
         ----------
@@ -44,17 +88,17 @@ class DataPreparation:
         return_list = [interactions_matrix, weights_matrix]
         
         if user_features is not None:
-            user_features_sp = self._get_user_features(user_features)
+            user_features_sp = self._get_user_features(OEM, user_features)
             return_list.append(user_features_sp)
 
         if item_features is not None:
-            item_features_sp = self._get_item_features(item_features)
+            item_features_sp = self._get_item_features(OEM, item_features)
             return_list.append(item_features_sp)
 
         return return_list
 
 
-    def _get_user_features(self, features):
+    def _get_user_features(self, OEM, features):
         """" Helper function to get the user_feature values and the user_feature list
 
         Returns
@@ -62,16 +106,20 @@ class DataPreparation:
         (user_features, user_tuple) : tuple(list, list)
             lists containing the data accordingly
         """
-    
-        user_features_terex = pd.read_csv('user_features_terex_new.csv', sep='|')
-        available_dealers = self.user_item[['user']].drop_duplicates()
-        user_features_terex = pd.merge(available_dealers, user_features_terex, left_on='user', right_on='dealer', how='left')
+        if OEM == 'AGCO':
+            user_features_file = pd.read_csv('user_features_agco_new.csv', sep='|')
+            user_features_file = self._map_dealer_size(user_features_file)
+        if OEM == 'TEREX':
+            user_features_file = pd.read_csv('user_features_terex_new.csv', sep='|')
         
-        user_features_ret = self._aggregate_features(user_features_terex, features, 'user')
+        available_dealers = self.user_item[['user']].drop_duplicates()
+        user_features_filtered = pd.merge(available_dealers, user_features_file, left_on='user', right_on='dealer', how='left')
+        
+        user_features_ret = self._aggregate_features(user_features_filtered, features, 'user')
 
         return user_features_ret
 
-    def _get_item_features(self, features):
+    def _get_item_features(self, OEM, features):
         """" Helper function to get the user_feature values and the user_feature list
 
         Returns
@@ -79,14 +127,19 @@ class DataPreparation:
         (user_features, user_tuple) : tuple(list, list)
             lists containing the data accordingly
         """
-    
-        items_terex = pd.read_csv('items_terex_new.csv', sep='|')
+        if OEM == 'AGCO':
+            items_file = pd.read_csv('items_agco_new.csv', sep='|')
+            skus_file = pd.read_csv('skus_agco_new.csv', sep='|', low_memory=False)
+        if OEM == 'TEREX':
+            items_file = pd.read_csv('items_terex_new.csv', sep='|')
+            skus_file = pd.read_csv('skus_terex_new.csv', sep='|', low_memory=False)
+        
         available_items = self.user_item[['item']].drop_duplicates()
         
-        item_features = pd.merge(available_items, items_terex, left_on='item', right_on='item_id', how='left')
-        skus_terex = pd.read_csv('skus_terex_new.csv', sep='|', low_memory=False)
+        items_filtered = pd.merge(available_items, items_file, left_on='item', right_on='item_id', how='left')
+        
 
-        item_sku_features = pd.merge(item_features, skus_terex, on='item_id', how='left')
+        item_sku_features = pd.merge(items_filtered, skus_file, on='item_id', how='left')
         
         item_features_ret = self._aggregate_features(item_sku_features, features, 'item')
 
@@ -104,14 +157,16 @@ class DataPreparation:
             ui_tuple = self._get_feature_tuple(uif_temp)
 
             dataset_t = Dataset()
-            dataset_t.fit(self.user_item.user.unique(), self.user_item.item.unique(), ui_features)
+            #dataset_t.fit(self.user_item.user.unique(), self.user_item.item.unique(), ui_features)
             
             if instance == 'user':
-                dataset_t.fit(self.user_item.user.unique(), self.user_item.item.unique(), user_features = ui_features)
+                #dataset_t.fit(self.user_item.user.unique(), self.user_item.item.unique(), user_features = ui_features)
+                dataset_t.fit(self.users, self.items, user_features = ui_features)
                 ui_features_sp = dataset_t.build_user_features(ui_tuple, normalize=False)
             
             if instance == 'item':
-                dataset_t.fit(self.user_item.user.unique(), self.user_item.item.unique(), item_features = ui_features)
+                #dataset_t.fit(self.user_item.user.unique(), self.user_item.item.unique(), item_features = ui_features)
+                dataset_t.fit(self.users, self.items, item_features = ui_features)
                 ui_features_sp = dataset_t.build_item_features(ui_tuple, normalize=False)
 
 
@@ -153,5 +208,13 @@ class DataPreparation:
         for i in range((add.shape[1]-add.shape[0])):
             csr = sparse.hstack((csr, add[:,(add.shape[0]+i)].A.T[0][:,None]))
         return csr
+
+    def _map_dealer_size(self, user_features):
+        user_features_temp = user_features.copy()
+        col = user_features_temp.item_count
+        bins = [col.min(), np.percentile(col, 25), np.percentile(col, 50), np.percentile(col, 75), col.max()]
+        names = ['1', '2', '3', '4']
+        user_features_temp['dealer_size'] = pd.cut(user_features_temp['item_count'], bins=bins, labels=names)
+        return user_features_temp
 
 
