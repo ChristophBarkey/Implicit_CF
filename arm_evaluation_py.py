5d4f96741d6afea0e131732d5635bb4b0ce1734a
ChristophBarkey <c_barkey@web.com>
2022-09-30 14:34:51 -0500

    bug fix

--- arm_evaluation_py.py
+++ arm_evaluation_py.py
@@ -10,7 +10,7 @@ def train_test_split_arm(data, split_date):
     test = data[data.create_date > split_date]
     test_filtered = filter_test_data(train, test)
     print('Train prop: ' + str(round(len(train) / len(data), 2)))
-    return (train, test)
+    return (train, test_filtered)
 
 
 
@@ -31,12 +31,12 @@ def filter_test_data(train, test):
 
 
 def filter_min_training_lines(train, test, arm_model):
-    num_training_lines_all = train.groupby('item_id')[['requested_quantity']].count().reset_index()
+    num_training_lines_all = train.groupby('item_id')[['user']].count().reset_index()
     min_training_lines = arm_model.min_support * train.co_id.nunique()
-    test_users = test.user.unique()
+    test_users = test.test_user.unique()
     for u in test_users:
-        test_subset = test[test.user == u]
-        training_lines_per_item = pd.merge(test_subset, num_training_lines_all, left_on='item_id', right_on='item_id', how='inner')
+        test_subset = test[test.test_user == u]
+        training_lines_per_item = pd.merge(test_subset, num_training_lines_all, left_on='test_items', right_on='item_id', how='inner')
         filtered_items = training_lines_per_item[training_lines_per_item.user > min_training_lines]
         if len(filtered_items) > 0:
             if u == test_users[0]:
