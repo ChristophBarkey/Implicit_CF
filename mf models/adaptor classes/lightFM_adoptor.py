8bf156fa881c1652fef04f7570eb9107f5325cdf
ChristophBarkey <c_barkey@web.com>
2022-11-11 17:19:52 +0100

    cleanup

--- mf models/adaptor classes/lightFM_adoptor.py
+++ mf models/adaptor classes/lightFM_adoptor.py
@@ -1,10 +1,10 @@
 # %%
 import numpy as np
-import multiprocessing
-
 from implicit.cpu.matrix_factorization_base import MatrixFactorizationBase
 
 class LightFMAdaptor(MatrixFactorizationBase):
+
+    # Class that contains wrapper functions for lightFM models to be evaluated with functions of the class implicit
     def __init__(self, iterations=1, random_state=22, *args, **kwargs):
         super(LightFMAdaptor, self).__init__()
 
@@ -36,16 +36,16 @@ class LightFMAdaptor(MatrixFactorizationBase):
             self.user_factors = self._transpose_features(user_features, self.model.user_embeddings, self.model.user_biases, 'user')
             self.item_factors = self._transpose_features(item_features, self.model.item_embeddings, self.model.item_biases, 'item')
 
-
+    # evluation function using the lightfm functions, to assure equal results of the implicit function and lightfm function
+    # Not used in thesis, only for testing this Class
     def evaluate(self, test_interactions, train_interactions, user_features, item_features, k=10):
-        from lightfm.evaluation import precision_at_k, recall_at_k, auc_score, reciprocal_rank
-
+        from lightfm.evaluation import precision_at_k
         pk = precision_at_k(model=self.model, test_interactions=test_interactions, train_interactions=train_interactions, 
         k=k, user_features=user_features, item_features=item_features).mean()
         return pk
 
 
-
+    # Function to aggregate user, item and potential feature factors to the equivalent user and item factors
     def _transpose_features(self, features, embeddings, biases, instance):
         if features is not None:
             num_ui = features.shape[0]
@@ -72,47 +72,5 @@ class LightFMAdaptor(MatrixFactorizationBase):
             if instance == 'item':
                 return np.concatenate((embeddings, np.ones((num_ui, 1)), biases.reshape(num_ui, 1)), axis=1).copy()
 
-
-    # deprecated by transpose_features!!
-    def _transpose_user_features(self, user_features, model):
-        if user_features is not None:
-            num_users = user_features.shape[0]
-            num_features = user_features.shape[1] - user_features.shape[0]
-            return_embeddings = model.user_embeddings[:num_users].copy()
-            feature_embedding = model.user_embeddings[num_users:].copy()
-            return_biases = model.user_biases[:num_users].copy()
-            feature_biases = model.user_biases[num_users:].copy()
-            for u in range(num_users):
-                for i in range(num_features):
-                    weight = user_features[u, num_users+i]
-                    return_embeddings[u] += weight * feature_embedding[i]
-                    return_biases[u] += weight * feature_biases[i]
-            return np.concatenate((return_embeddings, return_biases.reshape(num_users, 1), np.ones((num_users, 1))), axis=1).copy()
-        else:
-            num_users = model.user_embeddings.shape[0]
-            return np.concatenate((model.user_embeddings, model.user_biases.reshape(num_users, 1), np.ones((num_users, 1))), axis=1).copy()
-        
-
-    # deprecated by transpose_features!!
-    def _transpose_item_features(self, item_features, model):
-        if item_features is not None:
-            num_items = item_features.shape[0]
-            num_features = item_features.shape[1] - item_features.shape[0]
-            return_embeddings = model.item_embeddings[:num_items].copy()
-            feature_embedding = model.item_embeddings[num_items:].copy()
-            return_biases = model.item_biases[:num_items].copy()
-            feature_biases = model.item_biases[num_items:].copy()
-            for u in range(num_items):
-                for i in range(num_features):
-                    weight = item_features[u, num_items+i]
-                    return_embeddings[u] += weight * feature_embedding[i]
-                    return_biases[u] += weight * feature_biases[i]
-            return np.concatenate((return_embeddings, np.ones((num_items, 1)), return_biases.reshape(num_items, 1)), axis=1).copy()
-        else:
-            num_items = model.item_embeddings.shape[0]
-            return np.concatenate((model.item_embeddings, model.item_biases.reshape(num_items, 1), np.ones((num_items, 1))), axis=1).copy()
-
-    def save(self):
-        pass
 
 
